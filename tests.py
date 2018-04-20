b22f16ace0d7f511ae944a1ad613bc93053b3bdf
jhg14 <jonnygoffwhite@gmail.com>
2018-04-20 12:15:42 +0100

    Add test stubs

---
+++ tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+class TestTransformInstructions(unittest.TestCase):
+
+	def test_rename(self):
+		pass
+
+	def test_update(self):
+		pass
+
+	def test_append_first(self):
+		pass
+
+	def test_append(self):
+		pass
+
+	def test_insert_after(self):
+		pass
+
+	def test_move_first(self):
+		pass
+
+	def test_move_after(self):
+		pass
+
+	def test_remove(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
